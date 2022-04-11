f58ff6c8485fb4a6b07af78ab694d65bc48a4a2c
youngbin <redmangos@naver.com>
2022-04-11 22:05:44 +0900

    weather app city toggle

---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <h2>Weather Application</h2>
+    <p>{{ username }} 님 반갑습니다.</p>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+  background-image: linear-gradient(to right, #ffecd2 0%, #fcb69f 100%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+}
+.header h2 {
+  color: #fff;
+}
+
+.header p {
+  font-size: 12px;
+  color: #fff;
+}
+</style>
---
+++ src/components/weather/WeatherContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="weather-wrapper">
+    <ContentHeader />
+    <CitySelector />
+  </div>
+</template>
+
+<script>
+import CitySelector from "./CitySelector.vue";
+import ContentHeader from "./ContentHeader.vue";
+export default {
+  name: "WeatherContent",
+  components: {
+    ContentHeader,
+    CitySelector,
+  },
+};
+</script>
+
+<style></style>
