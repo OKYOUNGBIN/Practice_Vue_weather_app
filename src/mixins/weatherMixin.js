2d32308e7e3ba2a8554d4edf8b0ae3c630fcef48
youngbin <redmangos@naver.com>
2022-04-13 21:07:04 +0900

    weather app build

--- src/mixins/weatherMixin.js
+++ src/mixins/weatherMixin.js
@@ -6,7 +6,7 @@ const weatherMixin = {
   },
   methods: {
     async getWeatherInfo(city) {
-      const API_KEY = "9f804338792d432e9f0118545b44903a";
+      const API_KEY = process.env.VUE_APP_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.code}&appid=${API_KEY}`;
       const res = await axios.get(url);
       const { main, wind, weather } = res.data;
