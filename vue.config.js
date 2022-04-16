023208a35f84defe8c46f47733874357712c3e20
youngbin <redmangos@naver.com>
2022-04-17 00:25:21 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/Practice_Vue_weather_app",
-  outputDir: "docs",
+  publicPath: "/Practice_Vue_weather_app/",
+  outputDir: "./docs",
 });
